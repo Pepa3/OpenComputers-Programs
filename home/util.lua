4356909770dccfe8e8f98a692852983cfe56ded8
Pepa3 <SvobodaJosef117@gmail.com>
2022-01-23 20:22:58 +0100

    Language Project

--- home/util.lua
+++ home/util.lua
@@ -11,4 +11,29 @@ function export.genarr(x,y,def)
  return arr
 end
 
+function export.print1table(it,start,ende)
+  checkArg(1,it,"table")
+  checkArg(2,start,"number","nil")
+  checkArg(3,ende,"number","nil")
+  
+  start = start or 1
+  ende = ende or #it
+  for i=start,ende,1 do
+    print(it[i])
+  end
+end
+
+export.index = 0
+
+function export.offset()
+  export.index=export.index+1
+  return export.index
+end
+
+function export.reset()
+  local tmp = export.offset()
+  export.index=0
+  return tmp
+end
+
 return export
